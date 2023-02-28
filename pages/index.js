9eea82f9a1de65097a99276fc0759e3d3330299e
Chanaka Abeysinghe <chanakasan@gmail.com>
2023-02-28 11:02:16 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center mt-20">
+      <h1 className="text-2xl font-bold underline">Hello World</h1>
+    </div>
+  )
+}
